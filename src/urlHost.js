085dc0357b9d1a77dfb60f2378299849d262e0b7
Cash Myers <cmyers880@gmail.com>
2022-10-13 09:44:17 -0500

    fx: modified url to use local backend container.

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST =
     process.env.NODE_ENV === "production"
         ? "https://cors-everywhere-me.herokuapp.com/http://ytcbackend-env.eba-pztxhppp.us-east-2.elasticbeanstalk.com/"
-        : "http://127.0.0.1:8000";
+        : "http://127.0.0.1:80";
